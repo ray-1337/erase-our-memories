4c5712bd1f5f6978428bb90a49b6d9d74c753ec0
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-24 15:32:01 +0200

    added few configuration

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ const dotenv = require("dotenv");
 const secret = dotenv.config({path: process.cwd() + "/.env"});
 
 module.exports = {
-  token: secret.parsed["TOKEN"]
+  authorID: secret.parsed["AUTHOR"],
+  startMessageID: secret.parsed["STARTID"],
+  token: secret.parsed["TOKEN"],
+  channelID: secret.parsed["CHANNELID"]
 };
